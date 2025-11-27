96fca575561ee4c67676b2984c7df2b32ede13f2
Jakob Lingel <jlingel68@gmail.com>
2025-11-27 13:49:51 +0100

    Enable loading data from strapi

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,34 @@
+"use client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { fetchAPI } from "./utils/fetch-api";
 
-export default function Home() {
+type PropTypes = {
+  params: Promise<{ lang: string }>;
+};
+
+export default function Home({ params }: PropTypes) {
+  console.log("hello there");
+  const [data, setData] = useState<any>([]);
+
+  const fetchData = async () => {
+    try {
+      const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+      const path = `/homepage`;
+      const options = { headers: { Authorization: `Bearer ${token}` } };
+      const responseData = await fetchAPI(path, {}, options);
+      console.log(responseData);
+      setData(responseData.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,7 +42,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+          { data.Heading }
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
---
+++ src/proxy.ts
@@ -0,0 +1,41 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse, NextRequest } from "next/server";
+
+const locales = ["en-US", "de-DE", "de"];
+const defaultLocale = "de-DE";
+
+// Get the preferred locale based on Accept-Language header
+function getLocale(request: NextRequest): string {
+  const headers = {
+    "accept-language": request.headers.get("accept-language") || defaultLocale,
+  };
+  const languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function proxy(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale: string) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
