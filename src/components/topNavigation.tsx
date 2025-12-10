a9052972241df97fb6ed5728318aad95a83b9a7e
Jakob Lingel <jlingel68@gmail.com>
2025-12-10 20:15:39 +0100

    Add dark mode toggle

--- src/components/topNavigation.tsx
+++ src/components/topNavigation.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useEffect, useState } from "react";
 import MenuItems from "@/src/components/menuItems";
-import { Equal, X } from "lucide-react";
+import { Equal, Sun, X } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function TopNavigation() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     if (showMobileMenu) {
@@ -26,8 +28,12 @@ export default function TopNavigation() {
     return () => window.removeEventListener("resize", handleResize);
   }, [showMobileMenu]);
 
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <div className="m-4">
+    <div className="m-4 flex">
       {showMobileMenu && (
         <div className="p-4 md:hidden block mb-4 h-screen fixed inset-0 bg-black">
           <div
@@ -47,6 +53,9 @@ export default function TopNavigation() {
       </div>
       <div className="md:block hidden">
         <MenuItems />
+      </div>
+      <div className="ml-auto" onClick={toggleTheme}>
+        <Sun />
       </div>
     </div>
   );
