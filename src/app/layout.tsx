563748d769f7859b6fc308644b9e8024401ab2a0
Jakob Lingel <jlingel68@gmail.com>
2026-01-09 14:41:16 +0100

    Add analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Playfair_Display, Geist, Geist_Mono, Fira_Code } from "next/font/google";
+import {
+  Playfair_Display,
+  Geist,
+  Geist_Mono,
+  Fira_Code,
+} from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +31,10 @@ const firaCode = Fira_Code({
 export const metadata: Metadata = {
   title: {
     default: "Dr. Joy A. Alemazung - Bürgermeister von Heubach",
-    template: "%s | Dr. Joy A. Alemazung"
+    template: "%s | Dr. Joy A. Alemazung",
   },
-  description: "Dr. Joy A. Alemazung ist Bürgermeister von Heubach und Wissenschaftler mit Expertise in Nachhaltigkeit, Migrationspolitik und internationalen Beziehungen.",
+  description:
+    "Dr. Joy A. Alemazung ist Bürgermeister von Heubach und Wissenschaftler mit Expertise in Nachhaltigkeit, Migrationspolitik und internationalen Beziehungen.",
   keywords: [
     "Dr. Joy A. Alemazung",
     "Bürgermeister Heubach",
@@ -47,13 +54,14 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://alemazung.com",
     languages: {
-      'de': '/de',
-      'en': '/en',
+      de: "/de",
+      en: "/en",
     },
   },
   openGraph: {
     title: "Dr. Joy A. Alemazung - Bürgermeister von Heubach",
-    description: "Bürgermeister von Heubach und Wissenschaftler mit Expertise in Nachhaltigkeit, Migrationspolitik und internationalen Beziehungen.",
+    description:
+      "Bürgermeister von Heubach und Wissenschaftler mit Expertise in Nachhaltigkeit, Migrationspolitik und internationalen Beziehungen.",
     url: "https://alemazung.com",
     siteName: "Dr. Joy A. Alemazung",
     locale: "de_DE",
@@ -71,9 +79,13 @@ export default function RootLayout({
       <body
         className={`${playfairSerif.variable} ${geistSans.variable} ${geistMono.variable} ${firaCode.variable} antialiased`}
       >
-          {children}
+        <Script
+          src="https://analytics.jakob-lingel.dev/script.js"
+          data-website-id="80162986-705f-410d-b56e-ba2a61b9ae5e"
+          strategy="afterInteractive"
+        ></Script>
+        {children}
       </body>
     </html>
   );
 }
-
