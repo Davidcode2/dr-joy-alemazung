c329dbbb906aed01816c7ee9f2b179ff87cb6be4
Jakob Lingel <jlingel68@gmail.com>
2026-02-02 19:03:12 +0100

    Style fancy top navigation desktop

--- src/components/menu/desktopNav.tsx
+++ src/components/menu/desktopNav.tsx
@@ -58,7 +58,7 @@ function DropdownGroup({
 
       <div
         className={`
-          absolute left-0 top-full pt-2 z-50
+          absolute left-0 top-full pt-4 z-50
           transition-all duration-200 ease-out origin-top
           ${open
             ? "opacity-100 scale-y-100 translate-y-0"
@@ -66,12 +66,17 @@ function DropdownGroup({
           }
         `}
       >
-        <div className="bg-(--background) border border-(--ultralight-accent)/60 rounded-lg shadow-lg py-2 min-w-56">
+        <div className="bg-(--background)/90 border border-(--ultralight-accent)/60 rounded-b-lg shadow-lg backdrop-blur-md p-20 min-w-[800px]">
+          <div
+              className="text-[var(--muted-accent)]/80"
+          >
+            {group.label}
+          </div>
           {group.pages.map((page) => (
             <Link
               key={page.id}
               href={`/${locale}/${page.slug}`}
-              className="block px-4 py-2.5 transition-colors hover:bg-(--ultralight-accent)/40 hover:text-(--accent)"
+              className="font-serif text-4xl block py-2.5 transition-colors hover:text-(--accent)"
             >
               {page.heading}
             </Link>
